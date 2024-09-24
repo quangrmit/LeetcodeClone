41ffb51afe8d0ef2e7de0421a25b02049cb181f4
NguyenNguyen205 <s3927220@rmit.edu.vn>
2024-09-24 21:14:18 +0700

    Set up base backend

---
+++ backend/BusinessObjects/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    public class Question
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int QuestionId { get; set; }
+        [Required]
+        public string Content { get; set; }
+
+        public ICollection<Testcase> Testcases { get; } = new List<Testcase>();
+
+    }
+}
